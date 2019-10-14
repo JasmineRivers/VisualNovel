c6ccf1a80dfe80e5a91c928964c52c0c7537b00e
Jasmine <jazzykitkat@windowslive.com>
2019-10-14 18:33:13 +1300

    yay

---
+++ VisualNovel/Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class DialogueSystem : MonoBehaviour
+{
+    public static DialogueSystem instance;
+    TextSystem textSystem;
+    public ELEMENTS elements;
+    // public Text test;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textSystem = TextSystem.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkifTextIsFinished();
+    }
+
+    public void Say(string speech, string speaker = "")
+    {
+        StopSpeaking();
+        speaking = StartCoroutine(Speaking(speech, speaker));
+    }
+
+    public void StopSpeaking()
+    {
+        if (isSpeaking)
+        {
+            StopCoroutine(speaking);
+            
+        }
+        speaking = null;
+        
+    }
+
+    public bool isSpeaking { get { return speaking != null; } }
+    public bool isWatingForUserInput = false;
+
+
+    Coroutine speaking = null;
+    IEnumerator Speaking(string targetSpeech, string speaker = "")
+    {
+        speechPanel.SetActive(true);
+        speechText.text = "";
+        speakerNameText.text = DetermineSpeaker(speaker);
+        isWatingForUserInput = false;
+
+      while (speechText.text != targetSpeech)
+        {
+            speechText.text += targetSpeech[speechText.text.Length];
+            yield return new WaitForSeconds(0.1f);
+          
+        }
+
+        isWatingForUserInput = true;
+        while (isWatingForUserInput)
+        yield return new WaitForEndOfFrame();
+        textSystem.index++;
+        textSystem.userInput = false;
+        StopSpeaking();
+    }
+
+    public void SkipTextScroll(string targetSpeech, string speaker = "")
+    {
+        if (speaking != null)
+        {
+            speechText.text = targetSpeech;
+            StopCoroutine(speaking);
+            isWatingForUserInput = true;
+        }
+    }
+
+    string DetermineSpeaker(string speakerName)
+    {
+        string retVal = speakerNameText.text;
+        if (speakerName != speakerNameText.text && speakerName != "")
+            retVal = (speakerName.ToLower().Contains("narrator")) ? "" : speakerName;
+
+        return retVal;
+    }
+
+    void checkifTextIsFinished()
+    {
+        if (speaking == null)
+        {
+            textSystem.userInput = false;
+        }
+    }
+
+    [System.Serializable]
+    public class ELEMENTS
+    {
+        public GameObject speechPanel;
+        public Text speakerNameText;
+        public Text speechText;
+    }
+    public GameObject speechPanel { get { return elements.speechPanel; } }
+    public Text speakerNameText { get { return elements.speakerNameText; } }
+    public Text speechText { get { return elements.speechText; } }
+}
---
+++ VisualNovel/Assets/Scripts/TextSystem.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TextSystem : MonoBehaviour
+{
+    public static TextSystem instance;
+    DialogueSystem dialogue;
+    float waitTime = 0.0f;
+    float maxWaitTime = 0.7f;
+   [HideInInspector] public bool userInput = false;
+    //public AudioSource blink;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        instance = this;   
+    }
+    void Start()
+    {
+        dialogue = DialogueSystem.instance;
+        //blink = GetComponent<AudioSource>();
+    }
+
+
+    public string[] speakerText = new string[]
+        {
+            "Write Text in this box:AddNameHere"
+        };
+
+    [HideInInspector ]public int index = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogue != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Space) && userInput == false)
+        {
+
+                if (!dialogue.isSpeaking || dialogue.isWatingForUserInput)
+                {
+                    if (index >= speakerText.Length)
+                    {
+                        Debug.Log("Text,Done");
+
+                        return;
+                    }
+                    //blink.Play();
+                    Say(speakerText[index]);
+                    //index++;
+                }
+
+        }
+
+       
+            if (index < speakerText.Length)
+            {
+                stopSay(speakerText[index]);
+            }
+        }
+
+        Delay();
+
+
+    }
+
+    void Say(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1] : "";
+
+        dialogue.Say(speech, speaker);
+     
+    }
+
+    void stopSay(string s)
+    {
+        string[] parts = s.Split(':');
+        string speech = parts[0];
+        string speaker = (parts.Length >= 2) ? parts[1] : "";
+        SkipText(speech, speaker);
+    }
+
+    void SkipText(string speech, string speaker)
+    {
+            if (Input.GetKeyDown(KeyCode.Space) && !dialogue.isWatingForUserInput)
+            {
+                 if (userInput == true)
+                  {
+                      dialogue.SkipTextScroll(speech, speaker);
+                      index++;
+                      userInput = false;
+                      waitTime = 0;
+                  }
+            
+            }      
+    }
+
+    void Delay()
+    {
+        if (waitTime != maxWaitTime)
+        {
+            waitTime += Time.deltaTime;
+        }
+
+        if (waitTime >= maxWaitTime)
+        {
+            userInput = true;
+        }
+    }
+
+}
+
+
